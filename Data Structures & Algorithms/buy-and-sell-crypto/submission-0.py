a9145472ab1fea0993b530b09b920e5aba94238a
Rishabh Raj <70150570+Rishabh-Raj13@users.noreply.github.com>
2026-04-14 23:49:15 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit=0
+        for low in range(len(prices)-1):
+            for high in range(low+1,len(prices)):
+                if prices[low] < prices[high]:
+                    maxProfit = max(maxProfit,prices[high]-prices[low])
+                else:
+                    pass
+        return maxProfit
+
+
+        
